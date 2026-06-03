50f1e3e81acef235f90d243da3184f30cbbc3140
Sai Krishna <sai-krishna.peddinti-ext@hpe.com>
2026-06-03 13:58:37 +0530

    new initialization

---
+++ src/test/java/com/kaka/Kaka/KakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaka.Kaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
